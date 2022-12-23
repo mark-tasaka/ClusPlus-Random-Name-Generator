9458a49f0ee9477f45e34264b3863e140e5ea281
Mark Tasaka <mark.tasaka@gmail.com>
2022-12-23 15:29:59 -0800

    Update 1.02

---
+++ RandomNameGenerator/FemaleNameGiven.h
@@ -0,0 +1,68 @@
+#ifndef FEMALENAMEGIVEN_H
+#define FEMALENAMEGIVEN_H
+#include<iostream>
+#include <string>
+#include <vector>
+#include <stdlib.h>   
+#include <stdio.h>
+#include <time.h> 
+using namespace std;
+
+
+class FemaleNameGiven {
+public:
+	string africanGivenNamesFemale() const;
+	string africanAmericanGivenNamesFemale() const;
+	string americanGivenNamesFemale() const;
+	string arabicGivenNamesFemale() const;
+	string armenianGivenNamesFemale() const;
+	string celticGivenNamesFemale() const;
+	string chineseGivenNamesFemale() const;
+	string czechGivenNamesFemale() const;
+	string danishGivenNamesFemale() const;
+	string dutchGivenNamesFemale() const;
+	string egyptianGivenNamesFemale() const;
+	string englishGivenNamesFemale() const;
+	string finnishGivenNamesFemale() const;
+	string frenchGivenNamesFemale() const;
+	string gaelicGivenNamesFemale() const;
+	string germanGivenNamesFemale() const;
+	string greekGivenNamesFemale() const;
+	string hawaiianGivenNamesFemale() const;
+	string hebrewGivenNamesFemale() const;
+	string hindiGivenNamesFemale() const;
+	string hungarianGivenNamesFemale() const;
+	string indianGivenNamesFemale() const;
+	string irishGivenNamesFemale() const;
+	string italianGivenNamesFemale() const;
+	string japaneseGivenNamesFemale() const;
+	string koreanGivenNamesFemale() const;
+	string latinGivenNamesFemale() const;
+	string middleEnglishGivenNamesFemale() const;
+	string nativeAmericanGivenNamesFemale() const;
+	string norseGivenNamesFemale() const;
+	string oldEnglishGivenNamesFemale() const;
+	string oldFrenchGivenNamesFemale() const;
+	string oldGermanGivenNamesFemale() const;
+	string oldNorseGivenNamesFemale() const;
+	string persianGivenNamesFemale() const;
+	string polishGivenNamesFemale() const;
+	string polynesianGivenNamesFemale() const;
+	string russianGivenNamesFemale() const;
+	string sanskritGivenNamesFemale() const;
+	string scandinavianGivenNamesFemale() const;
+	string scottishGivenNamesFemale() const;
+	string slavicGivenNamesFemale() const;
+	string spanishGivenNamesFemale() const;
+	string swahiliGivenNamesFemale() const;
+	string swedishGivenNamesFemale() const;
+	string teutonicGivenNamesFemale() const;
+	string turkishGivenNamesFemale() const;
+	string vietnameseGivenNamesFemale() const;
+	string welshGivenNamesFemale() const;
+	string yiddishGivenNamesFemale() const;
+
+private:
+};
+
+#endif
---
+++ RandomNameGenerator/MaleNameGiven.h
@@ -0,0 +1,68 @@
+#ifndef MALENAMEGIVEN_H
+#define MALENAMEGIVEN_H
+#include<iostream>
+#include <string>
+#include <vector>
+#include <stdlib.h>   
+#include <stdio.h>
+#include <time.h> 
+using namespace std;
+
+
+class MaleNameGiven {
+public:
+	string africanGivenNamesMale() const;
+	string africanAmericanGivenNamesMale() const;
+	string americanGivenNamesMale() const;
+	string arabicGivenNamesMale() const;
+	string armenianGivenNamesMale() const;
+	string celticGivenNamesMale() const;
+	string chineseGivenNamesMale() const;
+	string czechGivenNamesMale() const;
+	string danishGivenNamesMale() const;
+	string dutchGivenNamesMale() const;
+	string egyptianGivenNamesMale() const;
+	string englishGivenNamesMale() const;
+	string finnishGivenNamesMale() const;
+	string frenchGivenNamesMale() const;
+	string gaelicGivenNamesMale() const;
+	string germanGivenNamesMale() const;
+	string greekGivenNamesMale() const;
+	string hawaiianGivenNamesMale() const;
+	string hebrewGivenNamesMale() const;
+	string hindiGivenNamesMale() const;
+	string hungarianGivenNamesMale() const;
+	string indianGivenNamesMale() const;
+	string irishGivenNamesMale() const;
+	string italianGivenNamesMale() const;
+	string japaneseGivenNamesMale() const;
+	string koreanGivenNamesMale() const;
+	string latinGivenNamesMale() const;
+	string middleEnglishGivenNamesMale() const;
+	string nativeAmericanGivenNamesMale() const;
+	string norseGivenNamesMale() const;
+	string oldEnglishGivenNamesMale() const;
+	string oldFrenchGivenNamesMale() const;
+	string oldGermanGivenNamesMale() const;
+	string oldNorseGivenNamesMale() const;
+	string persianGivenNamesMale() const;
+	string polishGivenNamesMale() const;
+	string polynesianGivenNamesMale() const;
+	string russianGivenNamesMale() const;
+	string sanskritGivenNamesMale() const;
+	string scandinavianGivenNamesMale() const;
+	string scottishGivenNamesMale() const;
+	string slavicGivenNamesMale() const;
+	string spanishGivenNamesMale() const;
+	string swahiliGivenNamesMale() const;
+	string swedishGivenNamesMale() const;
+	string teutonicGivenNamesMale() const;
+	string turkishGivenNamesMale() const;
+	string vietnameseGivenNamesMale() const;
+	string welshGivenNamesMale() const;
+	string yiddishGivenNamesMale() const;
+
+private:
+};
+
+#endif
