d9c6a36d0651a2ee65697a4fb6fee4c187ec8d24
Mark Tasaka <mark.tasaka@gmail.com>
2023-01-20 23:17:47 -0800

    Update 1.10

--- RandomNameGenerator/main.cpp
+++ RandomNameGenerator/main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include "Generator.h"
 #include "NameOrigins.h"
+#include<fstream>
+#include <iomanip>
 using namespace std;
 
 
@@ -11,6 +13,8 @@ int main()
 	int numberOfNames = 22;
 	int code = 25;
 	int code2 = 13;
+	ofstream file;
+	int input = 0;
 
 	Generator test1 = Generator();
 	vector<string> firstNames = test1.getFirstName(code, isMale);
@@ -31,6 +35,29 @@ int main()
 	cout << endl;
 	cout << "First name origins: " << firstNameOrigins << endl;
 	cout << "Surname origins: " << surnameOrigins << endl;
+
+	cout << endl;
+
+	cout << "Do you wish to create a text file?  Press '1' if you want a text file. " << endl;
+	cin >> input;
+
+	if (input == 1)
+	{
+		file.open("randomNames.txt");
+
+		for (int i = 0; i < numberOfNames; i++)
+		{
+			file << firstNames[i] << " " << surnames[i] << endl;
+		}
+
+		file << endl;
+		file << "First name origins: " << firstNameOrigins << endl;
+		file << "Surname origins: " << surnameOrigins << endl;
+
+		cout << "Text file created" << endl;
+	}
+
+
 
 
 	return 0;
